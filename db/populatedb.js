efb1073b793fe4ba92330276a51a489c1556abee
Huy Tran <huytrn13@gmail.com>
2026-06-19 13:15:48 -0500

    Seeding file

---
+++ db/populatedb.js
@@ -0,0 +1,49 @@
+const {Client} = require('pg');
+require('dotenv').config();
+
+const pokemonsTable = `
+    CREATE TABLE IF NOT EXISTS pokemons (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+        name VARCHAR(20) UNIQUE NOT NULL
+        image_path VARCHAR(255)
+        description TEXT
+    );
+
+    INSERT INTO pokemons (name, image_path, description)
+    VALUES
+        ('Bulbasaur', 'bulbasaur.png', 'It is one of the three First partner Pokémon that can be chosen in Kanto region, along with Squirtle and Charmander.'),
+        ('Squirtle', 'squirtle.png', 'It is one of the three First partner Pokémon that can be chosen in Kanto region, along with Bulbasaur and Charmander.') ,
+        ('Charmander', 'charmander.png', 'It is one of the three First partner Pokémon that can be chosen in the Kanto region, along with Bulbasaur and Squirtle.');
+`
+
+const typesTable = `
+    CREATE TABLE IF NOT EXISTS types (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
+        name VARCHAR(20) UNIQUE NOT NULL
+        image_path VARCHAR(255)
+    )
+`
+
+const trainersTable = `
+    CREATE TABLE IF NOT EXISTS trainers (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
+        name VARCHAR(20) UNIQUE NOT NULL
+        image_path VARCHAR(255)
+    )
+`
+
+
+async function main() {
+    console.log('Seeding...')
+
+    const client = new Client({
+        connectionString: process.env.DB_CONNECTION
+    })
+    await client.connect();
+    await client.query(pokemonsTable);
+    await client.end();
+
+    console.log('Seeding process completed.')
+}
+
+main();
