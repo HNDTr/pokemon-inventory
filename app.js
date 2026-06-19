4912e7b88443ccba60d1eb17b5dde0b3f3a45ea6
Huy Tran <huytrn13@gmail.com>
2026-06-19 12:24:21 -0500

    New pool connection

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+const PORT = 3000;
+app.listen(PORT, (err) => {
+    if (error) {
+        throw error
+    }
+    console.log(`Pokemon inventory server started - listening on port ${3000}`);
+})
